6aa39080486e7750234ff9dd10313506d0936a2e
Conrad Sanderson <conrad.sanderson@nicta.com.au>
2016-12-14 01:30:37 +1000

    version bump

--- include/armadillo_bits/diskio_meat.hpp
+++ include/armadillo_bits/diskio_meat.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2008-2015 National ICT Australia (NICTA)
+// Copyright (C) 2008-2016 National ICT Australia (NICTA)
 // 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this

f2bd6618625f5ece550b58787083b21e4cf102eb
conrad <conrad@localhost.localdomain>
2017-08-22 17:14:54 +1000

    restore previous version of conv()

--- include/armadillo_bits/glue_conv_meat.hpp
+++ include/armadillo_bits/glue_conv_meat.hpp
@@ -47,20 +47,6 @@ glue_conv::apply(Mat<eT>& out, const Mat<eT>& A, const Mat<eT>& B, const bool A_
     hh_mem[h_n_elem_m1-i] = h_mem[i];
     }
   
-  // construct HH matrix, with the column containing shifted versions of hh;
-  // upper limit for number of zeros is about 50%; may not be optimal
-  const uword N_copies = (std::min)(uword(10), h_n_elem); 
-  
-  const uword HH_n_rows = h_n_elem + (N_copies-1);
-  
-  Mat<eT> HH(HH_n_rows, N_copies, fill::zeros);
-  
-  for(uword i=0; i<N_copies; ++i)
-    {
-    arrayops::copy(HH.colptr(i) + i, hh.memptr(), h_n_elem);
-    }
-  
-  
   
   Col<eT> xx( (x_n_elem + 2*h_n_elem_m1), fill::zeros );  // zero padded version of x
   
@@ -74,39 +60,105 @@ glue_conv::apply(Mat<eT>& out, const Mat<eT>& A, const Mat<eT>& B, const bool A_
   
   eT* out_mem = out.memptr();
   
-  uword last_i      = 0;
-  bool  last_i_done = false;
-  
-  for(uword i=0; i < xx.n_elem; i += N_copies)
+  for(uword i=0; i < out_n_elem; ++i)
     {
-    if( ((i + HH_n_rows) <= xx.n_elem) && ((i + N_copies) <= out_n_elem) ) 
-      {
-      const Row<eT> xx_sub(xx_mem + i, HH_n_rows, false, true);
-      
-      Row<eT> out_sub(out_mem + i, N_copies, false, true);
-      
-      out_sub = xx_sub * HH;
-      
-      last_i_done = true;
-      }
-    else
-      {
-      last_i = i;
-      last_i_done = false;
-      break;
-      }
-    }
-  
-  if(last_i_done == false)
-    {
-    for(uword i=last_i; i < out_n_elem; ++i)
-      {
-      // out_mem[i] = dot( hh, xx.subvec(i, (i + h_n_elem_m1)) );
-      
-      out_mem[i] = op_dot::direct_dot( h_n_elem, hh_mem, &(xx_mem[i]) );
-      }
+    // out_mem[i] = dot( hh, xx.subvec(i, (i + h_n_elem_m1)) );
+    
+    out_mem[i] = op_dot::direct_dot( h_n_elem, hh_mem, &(xx_mem[i]) );
     }
   }
+
+
+
+// // alternative implementation of 1d convolution
+// template<typename eT>
+// inline
+// void
+// glue_conv::apply(Mat<eT>& out, const Mat<eT>& A, const Mat<eT>& B, const bool A_is_col)
+//   {
+//   arma_extra_debug_sigprint();
+//   
+//   const Mat<eT>& h = (A.n_elem <= B.n_elem) ? A : B;
+//   const Mat<eT>& x = (A.n_elem <= B.n_elem) ? B : A;
+//   
+//   const uword   h_n_elem    = h.n_elem;
+//   const uword   h_n_elem_m1 = h_n_elem - 1;
+//   const uword   x_n_elem    = x.n_elem;
+//   const uword out_n_elem    = ((h_n_elem + x_n_elem) > 0) ? (h_n_elem + x_n_elem - 1) : uword(0);
+//   
+//   if( (h_n_elem == 0) || (x_n_elem == 0) )  { out.zeros(); return; }
+//   
+//   
+//   Col<eT> hh(h_n_elem);  // flipped version of h
+//   
+//   const eT*   h_mem =  h.memptr();
+//         eT*  hh_mem = hh.memptr();
+//   
+//   for(uword i=0; i < h_n_elem; ++i)
+//     {
+//     hh_mem[h_n_elem_m1-i] = h_mem[i];
+//     }
+//   
+//   // construct HH matrix, with the column containing shifted versions of hh;
+//   // upper limit for number of zeros is about 50%; may not be optimal
+//   const uword N_copies = (std::min)(uword(10), h_n_elem); 
+//   
+//   const uword HH_n_rows = h_n_elem + (N_copies-1);
+//   
+//   Mat<eT> HH(HH_n_rows, N_copies, fill::zeros);
+//   
+//   for(uword i=0; i<N_copies; ++i)
+//     {
+//     arrayops::copy(HH.colptr(i) + i, hh.memptr(), h_n_elem);
+//     }
+//   
+//   
+//   
+//   Col<eT> xx( (x_n_elem + 2*h_n_elem_m1), fill::zeros );  // zero padded version of x
+//   
+//   const eT*  x_mem =  x.memptr();
+//         eT* xx_mem = xx.memptr();
+//   
+//   arrayops::copy( &(xx_mem[h_n_elem_m1]), x_mem, x_n_elem );
+//   
+//   
+//   (A_is_col) ? out.set_size(out_n_elem, 1) : out.set_size(1, out_n_elem);
+//   
+//   eT* out_mem = out.memptr();
+//   
+//   uword last_i      = 0;
+//   bool  last_i_done = false;
+//   
+//   for(uword i=0; i < xx.n_elem; i += N_copies)
+//     {
+//     if( ((i + HH_n_rows) <= xx.n_elem) && ((i + N_copies) <= out_n_elem) ) 
+//       {
+//       const Row<eT> xx_sub(xx_mem + i, HH_n_rows, false, true);
+//       
+//       Row<eT> out_sub(out_mem + i, N_copies, false, true);
+//       
+//       out_sub = xx_sub * HH;
+//       
+//       last_i_done = true;
+//       }
+//     else
+//       {
+//       last_i = i;
+//       last_i_done = false;
+//       break;
+//       }
+//     }
+//   
+//   if(last_i_done == false)
+//     {
+//     for(uword i=last_i; i < out_n_elem; ++i)
+//       {
+//       // out_mem[i] = dot( hh, xx.subvec(i, (i + h_n_elem_m1)) );
+//       
+//       out_mem[i] = op_dot::direct_dot( h_n_elem, hh_mem, &(xx_mem[i]) );
+//       }
+//     }
+//   }
 
 
 

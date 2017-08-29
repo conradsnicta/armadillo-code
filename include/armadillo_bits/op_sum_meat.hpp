601423797fe804ab4d55a9ae94147b97ef7d04f7
conrad <conrad@localhost.localdomain>
2017-08-29 23:19:46 +1000

    reduce pedantic compiler warnings

--- include/armadillo_bits/op_sum_meat.hpp
+++ include/armadillo_bits/op_sum_meat.hpp
@@ -240,6 +240,12 @@ op_sum::apply_noalias_proxy_mp(Mat<typename T1::elem_type>& out, const Proxy<T1>
         }
       }
     }
+  #else
+    {
+    arma_ignore(out);
+    arma_ignore(P);
+    arma_ignore(dim);
+    }
   #endif
   }
 
@@ -559,6 +565,12 @@ op_sum::apply_noalias_proxy_mp(Cube<typename T1::elem_type>& out, const ProxyCub
           }
         }
       }
+    }
+  #else
+    {
+    arma_ignore(out);
+    arma_ignore(P);
+    arma_ignore(dim);
     }
   #endif
   }

ae9432d4f7b696389bd5f06cc23b71746b53c370
conrad <conrad@localhost.localdomain>
2017-11-15 00:10:34 +1000

    simplification

--- include/armadillo_bits/fn_normpdf.hpp
+++ include/armadillo_bits/fn_normpdf.hpp
@@ -34,20 +34,6 @@ normpdf(const eT x)
 template<typename eT>
 inline
 typename enable_if2< (is_real<eT>::value), eT >::result
-normpdf(const eT x, const eT mu)
-  {
-  const eT tmp = (x - mu);
-  
-  const eT out = std::exp(-0.5 * (tmp*tmp)) / Datum<eT>::sqrt2pi;
-  
-  return out;
-  }
-
-
-
-template<typename eT>
-inline
-typename enable_if2< (is_real<eT>::value), eT >::result
 normpdf(const eT x, const eT mu, const eT sigma)
   {
   const eT tmp = (x - mu) / sigma;
@@ -101,63 +87,6 @@ normpdf(const Base<typename T1::elem_type, T1>& X_expr)
     for(uword i=0; i<N; ++i)
       {
       const eT tmp = X_mem[i];
-      
-      out_mem[i] = std::exp(-0.5 * (tmp*tmp)) / Datum<eT>::sqrt2pi;
-      }
-    }
-  
-  return out;
-  }
-
-
-
-template<typename T1, typename T2>
-inline
-typename enable_if2< (is_real<typename T1::elem_type>::value), Mat<typename T1::elem_type> >::result
-normpdf(const Base<typename T1::elem_type, T1>& X_expr, const Base<typename T1::elem_type, T2>& M_expr)
-  {
-  arma_extra_debug_sigprint();
-  
-  typedef typename T1::elem_type eT;
-  
-  const quasi_unwrap<T1> tmp1(X_expr.get_ref());
-  const quasi_unwrap<T2> tmp2(M_expr.get_ref());
-  
-  const Mat<eT>& X = tmp1.M;
-  const Mat<eT>& M = tmp2.M;
-  
-  arma_debug_check( (size(X) != size(M)), "normpdf(): size mismatch" );
-
-  Mat<eT> out( size(X) );
-  
-  eT* out_mem = out.memptr();
-  
-  const uword N     = X.n_elem;
-  const eT*   X_mem = X.memptr();
-  const eT*   M_mem = M.memptr();
-  
-  const bool use_mp = arma_config::cxx11 && arma_config::openmp && mp_gate<eT,true>::eval(N);
-  
-  if(use_mp)
-    {
-    #if defined(ARMA_USE_OPENMP)
-      {
-      const int n_threads = mp_thread_limit::get();
-      #pragma omp parallel for schedule(static) num_threads(n_threads)
-      for(uword i=0; i<N; ++i)
-        {
-        const eT tmp = (X_mem[i] - M_mem[i]);
-        
-        out_mem[i] = std::exp(-0.5 * (tmp*tmp)) / Datum<eT>::sqrt2pi;
-        }
-      }
-    #endif
-    }
-  else
-    {
-    for(uword i=0; i<N; ++i)
-      {
-      const eT tmp = (X_mem[i] - M_mem[i]);
       
       out_mem[i] = std::exp(-0.5 * (tmp*tmp)) / Datum<eT>::sqrt2pi;
       }

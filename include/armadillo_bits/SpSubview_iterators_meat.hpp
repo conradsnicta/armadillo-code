6a7cf8ae88425cd6f6c2dc1664461514457411d0
conrad <conrad@localhost.localdomain>
2018-05-01 11:38:21 +1000

    fix shadow declaration

--- include/armadillo_bits/SpSubview_iterators_meat.hpp
+++ include/armadillo_bits/SpSubview_iterators_meat.hpp
@@ -785,9 +785,9 @@ SpSubview<eT>::const_row_iterator::operator++()
     return *this;
     }
 
-  const uword aux_col = iterator_base::M->aux_col1;
-  const uword aux_row = iterator_base::M->aux_row1;
-  const uword n_cols = iterator_base::M->n_cols;
+  const uword aux_col  = iterator_base::M->aux_col1;
+  const uword aux_row  = iterator_base::M->aux_row1;
+  const uword M_n_cols = iterator_base::M->n_cols;
 
   // Otherwise, we need to search.  We have to loop over all of the columns in
   // the subview.
